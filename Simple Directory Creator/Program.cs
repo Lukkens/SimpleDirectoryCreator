036a09de3089672a4eb7aa2f5b3859e552f1ce22
stolij <stolijprogramming@gmail.com>
2018-06-11 09:15:42 +0200

    Add project files.

---
+++ Simple Directory Creator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple_Directory_Creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
