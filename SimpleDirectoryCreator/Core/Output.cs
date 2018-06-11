5333b4bf9e1e6ae5590bbcffa479e4ace9cda615
Lukkens <s93stijn.lukken@leerling.denieuwsteschool.nl>
2018-06-11 19:38:36 +0200

    Update Comment(s)

--- SimpleDirectoryCreator/Core/Output.cs
+++ SimpleDirectoryCreator/Core/Output.cs
@@ -5,7 +5,7 @@ namespace Simple_Directory_Creator.Core
 {
     public static class Output
     {
-        //From https://stackoverflow.com/questions/1649027/how-do-i-print-out-a-tree-structure
+        //Modified version of https://stackoverflow.com/questions/1649027/how-do-i-print-out-a-tree-structure
         public static void PrintTree(DirInfo root, string indent = "", bool last = true)
         {
             Console.Write(indent);
